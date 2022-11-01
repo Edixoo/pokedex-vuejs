d58db51531c603d5b2a57b2166e9e6936b0155d6
Paul <paul1pruvost@gmail.com>
2022-11-02 00:00:22 +0100

    Premiere étape faite

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -3,10 +3,30 @@
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  name: "app",
+  data() {
+    return{
+      name: "Paul",
+      pokemon: null,
+      img_pokemon: null
+    };
+  },
+  mounted () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then(response => (this.pokemon = response.results));
+    axios
+      .get("https://img.pokemondb.net/sprites/bank/normal/")
+      .then(response =>(this.img_pokemon = response))
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+
 .list {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
@@ -14,6 +34,7 @@ export default {};
   width: 100%;
   max-width: 510px;
 }
+
 article {
   height: 150px;
   background-color: #efefef;
@@ -23,9 +44,11 @@ article {
   cursor: pointer;
   box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
 }
+
 h3 {
   margin: 0;
 }
+
 #scroll-trigger {
   display: flex;
   justify-content: center;
@@ -40,5 +63,6 @@ img {
   width: 96px;
   height: 96px;
 }
+
 </style>
 
